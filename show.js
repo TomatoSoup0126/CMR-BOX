c302ffcb58fbd3265c4822cd11b71ad2d5a8bead
jonathantang <jonathantang0126@gmail.com>
2019-08-12 22:46:16 +0800

    use bootstrap-table

--- show.js
+++ show.js
@@ -16,15 +16,44 @@ var db = firebase.firestore()
 
 
 const listPanel = document.getElementById('area-lists')
+const dataPanel = document.getElementById('data - Panel')
+
 const freezer1 = [] //4度冰箱
 const freezer2 = [] //-20冰箱
 const freezer3 = [] //-80冰箱
+const dryingBox = [] //乾燥箱
+const liquidNitrogen = [] //液態氮桶
 
-
+let freezer4 = [{
+  area: "-80°C冰箱",
+  date: "2019-08-08",
+  location: "BOX255",
+  name: "Eco72I",
+  price: "1520",
+  quantity: "2",
+  resourse: "Thermo",
+  user: "Eva",
+}]
 
 getDocToArray('4°C冰箱', freezer1)
 getDocToArray('-20°C冰箱', freezer2)
 getDocToArray('-80°C冰箱', freezer3)
+getDocToArray('藥品乾燥箱', dryingBox)
+getDocToArray('液態氮桶', liquidNitrogen)
+
+
+var $table = $('#table')
+
+function creatSortingTable(data) {
+  $(function () {
+    $('#table').bootstrapTable({
+      data: data
+    })
+  })
+}
+
+
+
 
 //監聽list, 更動active項目, 取得類型ID, 傳入filterByGenres
 listPanel.addEventListener('click', (event) => {
@@ -36,7 +65,7 @@ listPanel.addEventListener('click', (event) => {
   }
 })
 
-
+//實現左側清單加上消除active的class
 function areaActive(event) {
   let areaArray = document.querySelectorAll('.list-group-item')
   for (let i = 0; i < areaArray.length; i++) {
@@ -45,6 +74,8 @@ function areaActive(event) {
   event.target.classList.add('active')
 }
 
+
+//把collection的doc寫入array中
 function getDocToArray(collectionName, arrayName) {
   db.collection(collectionName).get().then(function (querySnapshot) {
     querySnapshot.forEach(function (doc) {
